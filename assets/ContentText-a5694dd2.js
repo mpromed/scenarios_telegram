242f5ec9b09036a53faebdf06cf46ce6cf8f87be
Averjahn <ogoka2001@gmail.com>
2024-10-21 16:22:17 +0300

    upd

--- assets/ContentText-a5694dd2.js
+++
@@ -1,1 +0,0 @@
-import{u as o,o as s,c}from"./index-6ca8b39f.js";const a=["innerHTML"],l={__name:"ContentText",props:["componentData","testStyle"],setup(t){const e=t;return o(n=>({"64fbb36c":t.testStyle.textColor})),(n,_)=>(s(),c("div",{innerHTML:e.componentData.text,class:"content-text"},null,8,a))}};export{l as default};
