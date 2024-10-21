8e6911cbd13ca46dd45fe73420c9fedb46cf3993
Averjahn <ogoka2001@gmail.com>
2024-10-21 16:41:31 +0300

    upda

---
+++ assets/ContentText-d65b025f.js
@@ -0,0 +1,1 @@
+import{u as o,o as s,c}from"./index-aa1cfb93.js";const a=["innerHTML"],l={__name:"ContentText",props:["componentData","testStyle"],setup(t){const e=t;return o(n=>({"64fbb36c":t.testStyle.textColor})),(n,_)=>(s(),c("div",{innerHTML:e.componentData.text,class:"content-text"},null,8,a))}};export{l as default};
--- assets/answerCheckMark-36398278.js
+++
@@ -1,1 +0,0 @@
-const r=""+new URL("answerCheckMark-41728157.svg",import.meta.url).href;export{r as _};
