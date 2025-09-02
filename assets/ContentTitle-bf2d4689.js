f561173b66a0af60860dc5720c1ed6746ec9ba48
Averjahn <ogoka2001@gmail.com>
2025-09-02 16:48:03 +0300

    upd

---
+++ assets/ContentTitle-bf2d4689.js
@@ -0,0 +1,1 @@
+import{o as n,c as o}from"./index-cad0f813.js";const c=["innerHTML"],l={__name:"ContentTitle",props:["componentData","testStyle"],setup(t){const e=t;return(s,_)=>(n(),o("div",{innerHTML:e.componentData.title,class:"content-title"},null,8,c))}};export{l as default};
--- assets/ContentTitle-ca08e16b.js
+++
@@ -1,1 +0,0 @@
-import{o as n,c as o}from"./index-2bfbddc2.js";const c=["innerHTML"],l={__name:"ContentTitle",props:["componentData","testStyle"],setup(t){const e=t;return(s,_)=>(n(),o("div",{innerHTML:e.componentData.title,class:"content-title"},null,8,c))}};export{l as default};
