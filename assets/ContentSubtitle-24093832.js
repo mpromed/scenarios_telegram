f14fbef8843d1b70d468a707682fc0a7e66fa44c
Averjahn <ogoka2001@gmail.com>
2025-09-02 14:43:28 +0300

    last upd

--- assets/ContentSubtitle-24093832.js
+++
@@ -1,1 +0,0 @@
-import{u as s,o,c}from"./index-39085bd7.js";const l=["innerHTML"],_={__name:"ContentSubtitle",props:["componentData","testStyle"],setup(t){const e=t;return s(n=>({c2cee292:t.testStyle.subtitleColor})),(n,a)=>(o(),c("div",{innerHTML:e.componentData.subtitle,class:"content-subtitle"},null,8,l))}};export{_ as default};
