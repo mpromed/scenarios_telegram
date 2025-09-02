44bdd405e47da9246c90e77e1f16eb03d6435cf5
Averjahn <ogoka2001@gmail.com>
2025-09-02 20:03:36 +0300

    updtg

--- assets/ContentTitle-f6af45d1.js
+++
@@ -1,1 +0,0 @@
-import{o as n,c as o}from"./index-30d61e39.js";const c=["innerHTML"],l={__name:"ContentTitle",props:["componentData","testStyle"],setup(t){const e=t;return(s,_)=>(n(),o("div",{innerHTML:e.componentData.title,class:"content-title"},null,8,c))}};export{l as default};
