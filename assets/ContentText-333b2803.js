5d00bcbe3dc703fc36bb6cf6b0a8018c294b4611
Averjahn <ogoka2001@gmail.com>
2025-09-02 12:50:46 +0300

    upf

--- assets/ContentText-333b2803.js
+++
@@ -1,1 +0,0 @@
-import{u as o,o as s,c}from"./index-4b46acd1.js";const a=["innerHTML"],l={__name:"ContentText",props:["componentData","testStyle"],setup(t){o(n=>({"64fbb36c":t.testStyle.textColor}));const e=t;return(n,_)=>(s(),c("div",{innerHTML:e.componentData.text,class:"content-text"},null,8,a))}};export{l as default};
