20d80f4acfac5e6472b957e8a982ab2f8d0f65fb
Averjahn <ogoka2001@gmail.com>
2025-09-02 18:27:25 +0300

    tgupd

---
+++ assets/ContentTitle-4fff1468.js
@@ -0,0 +1,1 @@
+import{o as n,c as o}from"./index-d3c718af.js";const c=["innerHTML"],l={__name:"ContentTitle",props:["componentData","testStyle"],setup(t){const e=t;return(s,_)=>(n(),o("div",{innerHTML:e.componentData.title,class:"content-title"},null,8,c))}};export{l as default};
